4d5c9abeef10b2e7d21935808c9a6ba5dbba44df
Viet Nguyen <vmnguyen@ryerson.ca>
2019-07-13 19:49:27 -0400

    added Navbar component with stylings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Routes from "./directory/Routes";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes />
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./header.styles.scss";
+import { NavLink, Link } from "react-router-dom";
+import { ReactComponent as Logo } from "./crown.svg";
+
+export default function Navbar() {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+
+      <div className="options">
+        <NavLink to="/shop" className="option" activeClassName="active">
+          SHOP
+        </NavLink>
+        <NavLink to="/contact" className="option" activeClassName="active">
+          CONTACT
+        </NavLink>
+        <NavLink to="/signin" className="option" activeClassName="active">
+          SIGN IN
+        </NavLink>
+        <NavLink to="/cart" className="option" activeClassName="active">
+          CART
+        </NavLink>
+      </div>
+    </div>
+  );
+}
