4c4656dd35858ec261a15af3b1352bda2bb2a359
Viet Nguyen <vmnguyen@ryerson.ca>
2019-07-26 21:53:34 -0400

    added "Add to Card" button to each collection item

--- src/components/CollectionItem.js
+++ src/components/CollectionItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./collection-item.styles.scss";
+import CustomButton from "./CustomButton";
 
 export default function CollectionItem({ name, imageUrl, price }) {
   return (
@@ -9,6 +10,9 @@ export default function CollectionItem({ name, imageUrl, price }) {
         <span>{name}</span>
         <span>{price}</span>
       </div>
+      <CustomButton inverted className="custom-button">
+        ADD TO CART
+      </CustomButton>
     </div>
   );
 }
