f0fed6beeb0843bc0c52fb1f44c6cb606fe01beb
Viet Nguyen <vmnguyen@ryerson.ca>
2019-07-28 17:56:03 -0400

    added items count to Cart component

--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -4,15 +4,23 @@ import "./cart-icon.scss";
 import { toggleCartDropdown } from "../redux/cart/cart.actions";
 import { connect } from "react-redux";
 
-function CartIcon({ toggleCartDropdown }) {
+function CartIcon({ toggleCartDropdown, totalItems }) {
   return (
     <div className="cart-icon" onClick={toggleCartDropdown}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{totalItems}</span>
     </div>
   );
 }
 
+const mapStateToProps = state => {
+  return {
+    totalItems: state.cart.cartItems.reduce(
+      (acc, item) => acc + item.quantity,
+      0
+    )
+  };
+};
 const mapDispatchToProps = dispatch => {
   return {
     toggleCartDropdown: () => dispatch(toggleCartDropdown())
@@ -20,6 +28,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
