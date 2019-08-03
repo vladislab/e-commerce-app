c80455ee72ad9ef6625b2667e663bb815b79b115
Viet Nguyen <vmnguyen@ryerson.ca>
2019-08-03 19:04:51 -0400

    Updates

---
+++ precache-manifest.638aeef107ce72deca3d92e4f8a90dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d249ea3b0169e6b5c7d0e9b89d2292ec",
+    "url": "/e-commerce-app/index.html"
+  },
+  {
+    "revision": "4e5f9aee24b3f79fa626",
+    "url": "/e-commerce-app/static/css/main.dea91328.chunk.css"
+  },
+  {
+    "revision": "7793ad0702813311eab9",
+    "url": "/e-commerce-app/static/js/2.42ea8ce6.chunk.js"
+  },
+  {
+    "revision": "4e5f9aee24b3f79fa626",
+    "url": "/e-commerce-app/static/js/main.d3fc68eb.chunk.js"
+  },
+  {
+    "revision": "632a788f4bd551d79aae",
+    "url": "/e-commerce-app/static/js/runtime~main.552b0c5d.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce-app/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce-app/static/media/shopping-bag.30f07071.svg"
+  }
+]);
