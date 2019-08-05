4dd98619304982cb618099034537409c93ac127e
Viet Nguyen <vmnguyen@ryerson.ca>
2019-08-05 16:21:44 -0400

    disable middlewares in production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,9 @@ import rootReducer from "./root.reducer";
 
 const middlewares = [logger];
 
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 const persistor = persistStore(store);
